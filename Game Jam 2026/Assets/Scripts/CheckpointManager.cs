7b0498ad5179d2bb7be222e393ff1ba0cfebd196
BigJon00 <anime4dahomies@gmail.com>
2026-01-31 17:54:08 -0500

    Respawn.cs, checkpoint.cs, checkpointmanager.cs

---
+++ Game Jam 2026/Assets/Scripts/CheckpointManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    public static CheckpointManager instance;
+
+    private Vector3 savedPosition;
+    private Quaternion savedRotation;
+    private bool hasCheckpoint = false;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetCheckpoint(Vector3 pos, Quaternion rot)
+    {
+        savedPosition = pos;
+        savedRotation = rot;
+        hasCheckpoint = true;
+        Debug.Log("Checkpoint saved at: " + savedPosition);
+    }
+
+    public void RespawnPlayer(Transform player)
+    {
+        if (!hasCheckpoint)
+        {
+            Debug.Log("No checkpoint set yet!");
+            return;
+        }
+
+        player.position = savedPosition;
+        player.rotation = savedRotation;
+
+        Rigidbody rb = player.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
